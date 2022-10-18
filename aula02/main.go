adf71a06b9b3776b425194f122e093063f014203
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-10-18 20:37:10 -0300

    aula 02

--- aula02/main.go
+++ aula02/main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+//guiado
 const (
 	minimum = "minimum"
 	average = "average"
 	maximum = "maximum"
- )
+)
+
+
 
 func main() {
 
@@ -35,7 +38,13 @@ func main() {
 	fmt.Println("Valor maximo:", maxValue)
 
 	//mesa
+	salarioEmpregadoA, _ := calcularSalario(600, "A")
+	salarioEmpregadoB, _ := calcularSalario(600, "B")
+	salarioEmpregadoC, _ := calcularSalario(600, "C")
 
+	fmt.Println("Salario do empregado A:", salarioEmpregadoA)
+	fmt.Println("Salario do empregado B:", salarioEmpregadoB)
+	fmt.Println("Salario do empregado C:", salarioEmpregadoC)
 
 }
 
@@ -50,7 +59,7 @@ func operation(op string) (func(i ...int) int, error) {
 		return maxCalc, nil
 	}
 
-	return nil, fmt.Errorf("operetion not allowed")
+	return nil, fmt.Errorf("operation not allowed")
 }
 
 func minCalc(i ...int) int {
@@ -82,3 +91,25 @@ func maxCalc(i ...int) int {
 }
 
 //mesa
+func calcularSalario(minutos float32, categoria string) (float32, error) {
+	const (
+		A = 112.38
+		B = 56.19
+		C = 37.46
+	)
+	var horas float32 = minutos/60
+
+	switch categoria {
+	case "A":
+		return multiplicar(horas, A, 0.5), nil
+	case "B":
+		return multiplicar(horas, B, 0.2), nil
+	case "C":
+		return multiplicar(horas, C, 0), nil
+	}
+	return 0.0, fmt.Errorf("Erro no calculo do salario. Verifique os parametros inseridos.")
+}
+
+func multiplicar(horas float32, valorPorHora float32, porcentagem float32) float32 {
+	return horas * valorPorHora * (1 + porcentagem)
+}
