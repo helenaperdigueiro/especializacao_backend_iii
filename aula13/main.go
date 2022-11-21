78f7028b12580c35066e9f8e2ef2c83bc97542d1
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-11-21 20:27:28 -0300

    aula13

---
+++ aula13/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Product struct {
+	Id          string  `json:"id"`
+	Name        string  `json:"name"`
+	Price       float64 `json:"price"`
+	Inventory   string  `json:"inventory"`
+	Code        string  `json:"code"`
+	IsPublished bool    `json:"isPublished"`
+	CreatedAt   string  `json:"createdAt"`
+}
+
+func main() {
+	app := gin.Default()
+
+	res, err := os.ReadFile("./products.json")
+	if err != nil {
+		panic("Could not read file.")
+	}
+
+	var products []Product
+
+	err = json.Unmarshal(res, &products)
+	if err != nil {
+		panic(err)
+	}
+
+	app.GET("/products", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, products)
+	})
+
+	app.Run(":8080")
+}
